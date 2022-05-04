b5ee3ebf84ab5b1d2b9d84bf0ebf30acd472ac87
NadaAlhabbash <nadaalhabbash@gmail.com>
2022-05-04 11:04:38 +0300

    I

--- app/Models/Policlinic.php
+++ app/Models/Policlinic.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Policlinic extends Model
 {
     use HasFactory;
+    #Many To One
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/admin/policlinic/index.blade.php
+++ resources/views/admin/policlinic/index.blade.php
@@ -42,7 +42,7 @@
                                      @foreach($data as $rs)
                                      <tr>
                                          <td>{{$rs->id}}</td>
-                                         <td>{{\App\Http\Controllers\AdminPanel\CategoryController::getParentsTree($rs, $rs->title)}}</td>
+                                         <td>{{\App\Http\Controllers\AdminPanel\CategoryController::getParentsTree($rs->category, $rs->category->title)}}</td>
                                          <td>{{$rs->title}}</td>
                                          <td>{{$rs->specialization}}</td>
                                          <td>{{$rs->location}}</td>
