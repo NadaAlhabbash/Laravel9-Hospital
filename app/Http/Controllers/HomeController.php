1d6f5f7e5d92d8621ea83664949a89c7cf602670
NadaAlhabbash <nadaalhabbash@gmail.com>
2022-05-12 11:08:43 +0300

    ShowDataInMainPage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,8 +17,10 @@ class HomeController extends Controller
 
         $sliderdata=Policlinic::limit(4)->get();
 //        dd($sliderdata);
+        $policlinicList=Policlinic::limit(6)->get();
         return view('home.index', [
-            'sliderdata'=>$sliderdata
+            'sliderdata'=>$sliderdata,
+            'policlinicList'=>$policlinicList
         ]);
     }
 
